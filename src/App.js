ea9e9a8d7d66688cc140b098674c70d4a9266491
Azkary Garcia <macbookpro@Azkarys-MacBook-Pro.local>
2020-05-03 18:18:53 -0500

    Sets the App for the front end with the routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      Hello world!
-    </div>
+    <Router> //Usa el browserRouter para asignar las rutas a distintos componentes
+      <Navbar /> //Manda a llamar el componente Navbar
+      <br /> //Agrega un line break
+      <Route path="/" exact component={ExercisesList} /> //Manda a llamar el componente ExercisesList cuando vamos a la ruta "/"
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router> 
   );
 }
 
